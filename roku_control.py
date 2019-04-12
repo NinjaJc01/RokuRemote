d557b37c8b12c46ad070b2336abab3284bfe1632
James <james@cookiehome.co.uk>
2019-04-12 16:51:58 +0100

    Delete roku_control.py

--- roku_control.py
+++
@@ -1,7 +0,0 @@
-import requests
-ROKU_URL = "http://192.168.1.10:8060/"
-
-def sendKeypress(key_string):
-    a = requests.post((ROKU_URL+"keypress/"+key_string))
-    print(a.status_code)
-sendKeypress("play")
